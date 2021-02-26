fcd2fd24ddd29f9d3d75e928923e4e56a64e62c4
ThoLes94 <59646128+ThoLes94@users.noreply.github.com>
2021-02-26 16:59:14 +0100

    option fin

--- Projet/register/PrintEventForm.php
+++ Projet/register/PrintEventForm.php
@@ -2,20 +2,29 @@
 function printFormEvent($idEvent, $dbh){
     $prenom=$_SESSION['prenom'];
     $event = Event::getEvenement($dbh, $idEvent);
-    echo "<p> Bonjour $prenom, </p>";
-    echo $idEvent;
+    echo "<div> <p> Bonjour $prenom, </p>";
     $login=$_SESSION['login'];
 
     echo "<p> Nous sommes ravis de voir que vous vous intéressez à cet événement, remplissez le formulaire ci-dessous pour confirmer votre inscription </p>";
     echo <<<CHAINE_DE_FIN
+    <div class= "col-md-3"></div><div class= "form col-md-6 w3-card w3-round w3-padding">
     <form action="index.php?todo=inscription_cours&page=inscription_cours" method="post">
     <p>Evénemment : <input type="int" required name='EventTitle' value='$event->title' disabled="disabled"/></p>
     <p>Nom d'utilisateur : <input type="text"  value='$login' disabled="disabled" required name="login"/></p>
     <p>Adresse e-mail : <input type="email" name="email" required /></p>
     <p>Mot de passe : <input type="password" name="mdp" required name='mdp' /></p>
     <span class="w3-hide"><input id="idevent" type="text" name="idevent"  value='$idEvent'  required></span>
+    <p> Niveau :
+                <select name="niveau" required>
+                    <option value=""></option>
+                    <option value="1">Débutant</option>
+                    <option value="2">Intermédiaire</option>
+                    <option value="3">Fort</option>
+                </select>
+            </p>
     <p><input type="submit" value="Valider" /></p>
     </form>
+    </div><div class= "col-md-3"></div>
 CHAINE_DE_FIN;
     }
     ?>
